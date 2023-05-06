dcfb86985abf92231078631b177861c3f5176af9
Dawid <tetrin22@gmail.com>
2023-05-06 12:49:07 +0200

    Created site

---
+++ js/main.js
@@ -0,0 +1,60 @@
+const nav = document.querySelector(".nav");
+const navBtn = document.querySelector(".burger-btn");
+const allNavItems = document.querySelectorAll(".nav__item");
+
+const navBtnBars = document.querySelector(".burger-btn__bars");
+const allSections = document.querySelectorAll(".section");
+const footerYear = document.querySelector(".footer__year");
+
+const handleNav = () => {
+	nav.classList.toggle("nav--active");
+
+    navBtnBars.classList.remove('black-bars-color')
+
+	allNavItems.forEach((item) => {
+		item.addEventListener("click", () => {
+			nav.classList.remove("nav--active");
+		});
+	});
+
+	handleNavItemsAnimation();
+};
+
+const handleNavItemsAnimation = () => {
+	let delayTime = 0;
+
+	allNavItems.forEach((item) => {
+		item.classList.toggle("nav-items-animation");
+		// animation-delay: .3s; to samo na dole
+		item.style.animationDelay = "." + delayTime + "s";
+		delayTime++;
+	});
+};
+
+const handleObserver = () => {
+	const currentSection = window.scrollY;
+
+	allSections.forEach((section) => {
+		if (
+			section.classList.contains("white-section") &&
+			section.offsetTop <= currentSection + 60
+		) {
+			// tutaj sprawdzamy czy nasza górna krawędź strony weszła już na nową sekjcę (60 to wysokość white-block)
+			navBtnBars.classList.add("black-bars-color");
+		} else if (
+			!section.classList.contains("white-section") &&
+			section.offsetTop <= currentSection + 20
+		) {
+			navBtnBars.classList.remove("black-bars-color");
+		}
+	});
+};
+
+const handleCurrentYear = () => {
+	const year = new Date().getFullYear();
+	footerYear.innerText = year;
+};
+
+handleCurrentYear();
+navBtn.addEventListener("click", handleNav);
+window.addEventListener("scroll", handleObserver);
